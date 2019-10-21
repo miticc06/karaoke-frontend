cf3a07b744b52df922dfa077cbfde7972789f5f6
Dang Minh Tien <dangminhtien06@gmail.com>
2019-10-21 21:34:52 +0700

    Add Mobx

---
+++ src/config/auth.js
@@ -0,0 +1,18 @@
+import { action, observable } from 'mobx'
+
+class AuthStore {
+  @observable isAuth = !!window.localStorage.getItem('access-token')
+
+  @action
+  authenticate = token => {
+    window.localStorage.setItem('access-token', token)
+    this.isAuth = true
+  }
+
+  logout = () => {
+    window.localStorage.clear()
+    this.isAuth = false
+  }
+}
+
+export default AuthStore
