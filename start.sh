8f4209ee35977697393f08f60f6918c200a30f9e
Đặng Minh Tiến <dangminhtien06@gmail.com>
2019-11-13 13:14:26 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 cd /home/deploy/karaoke-frontend
-docker build --rm -f Dockerfile -t rick-morty-random-episode:latest .
-docker run --rm -d -p 80:80 rick-morty-random-episode:latest
+docker rm $(docker stop $(docker ps -a -q -f ancestor=frontend))
+docker build --rm -f Dockerfile -t frontend .
+docker run --rm -d -p 80:80 frontend
