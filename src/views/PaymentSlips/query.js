ee65847fae5e01b04eb75de7a1680acd72f3b78a
catrom <thanhluanqn1998@gmail.com>
2019-11-22 21:15:41 +0700

    add Payment Slip FE

---
+++ src/views/PaymentSlips/query.js
@@ -0,0 +1,71 @@
+import gql from 'graphql-tag'
+
+export const GET_PAYMENTSLIPS = gql`
+  query {
+    paymentSlips {
+      _id
+      description
+      price
+      createdAt
+      createdBy {
+        name
+      }
+    }
+  }
+`
+
+export const GET_PAYMENTSLIP = gql`
+  query ($paymentSlipId: ID!) {
+    paymentSlip(paymentSlipId: $paymentSlipId) {
+      _id
+      description
+      price
+      createdAt
+      createdBy {
+        name
+      }
+    }
+  }
+`
+
+export const ADD_PAYMENTSLIP = gql`
+  mutation ($input: PaymentSlipInput!){
+    createPaymentSlip(input: $input) {
+      _id
+      description
+      price
+      createdAt
+      createdBy {
+        name
+      }
+    }
+  }
+`
+
+export const UPDATE_PAYMENTSLIP = gql`
+  mutation ($paymentSlipId: String!, $input: PaymentSlipInput!){
+    updatePaymentSlip(paymentSlipId: $paymentSlipId,input: $input) {
+      _id
+      description
+      price
+      createdAt
+      createdBy {
+        name
+      }
+    }
+  }
+`
+
+export const DELETE_PAYMENTSLIP = gql`
+  mutation($paymentSlipId: String!) {
+    deletePaymentSlip(paymentSlipId: $paymentSlipId) {
+      _id
+      description
+      price
+      createdAt
+      createdBy {
+        name
+      }
+    }
+  }
+`
